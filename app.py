81a98cfd38c332dced18cfb78d765a4c1c376087
my-flask-app <zlongle362@gmail.com>
2025-11-22 22:24:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,12 @@ def admin():
 if __name__ == "__main__":
     # Chạy web trên localhost:5000
     app.run(debug=True, host="127.0.0.1", port=5000)
+    from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
